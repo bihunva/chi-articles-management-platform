8d2268bd6bddf12a6730df5ef678ca13bac24084
Vadym Bihun <bihunva@gmail.com>
2024-12-03 23:50:45 +0200

    Fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,74 +1,36 @@
+from unittest.mock import patch
+
 import pytest
-from flask import url_for
 
-from app.models.user import User, Role
-
-
-@pytest.fixture
-def admin_user_data():
-    return {"username": "admin_user", "password": "admin_password"}
+from app import create_app
+from app import db as _db
+from app.config import Config
 
 
-@pytest.fixture
-def editor_user_data():
-    return {"username": "editor_user", "password": "editor_password"}
+@pytest.fixture(scope="function")
+def app():
+    Config.SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    app = create_app()
+
+    with app.app_context():
+        _db.create_all()
+        yield app
+        _db.drop_all()
 
 
-@pytest.fixture
-def viewer_user_data():
-    return {"username": "viewer_user", "password": "viewer_password"}
+@pytest.fixture(scope="function")
+def client(app):
+    return app.test_client()
 
 
-@pytest.fixture
-def admin_user(client, db, admin_user_data):
-    register_response = register_user(client, admin_user_data)
-    update_user_role(db, admin_user_data["username"], "admin")
-    access_token = get_access_token(client, admin_user_data)
-    return {
-        "id": register_response.get_json()["id"],
-        "user_data": admin_user_data,
-        "access_token": access_token,
-    }
+@pytest.fixture(scope="function")
+def mock_redis():
+    with patch("app.utils.auth.redis_client") as mock_redis_client:
+        yield mock_redis_client
 
 
-@pytest.fixture
-def editor_user(client, db, editor_user_data):
-    register_response = register_user(client, editor_user_data)
-    update_user_role(db, editor_user_data["username"], "editor")
-    access_token = get_access_token(client, editor_user_data)
-    return {
-        "id": register_response.get_json()["id"],
-        "user_data": editor_user_data,
-        "access_token": access_token,
-    }
-
-
-@pytest.fixture
-def viewer_user(client, db, viewer_user_data):
-    register_response = register_user(client, viewer_user_data)
-    access_token = get_access_token(client, viewer_user_data)
-    return {
-        "id": register_response.get_json()["id"],
-        "user_data": viewer_user_data,
-        "access_token": access_token,
-    }
-
-
-def update_user_role(db, username, role):
-    user = User.query.filter_by(username=username).first()
-    user.role = Role[role]
-    db.session.commit()
-
-
-def register_user(client, user_data):
-    return client.post(url_for("Auth.register_view"), json=user_data)
-
-
-def login_user(client, user_data):
-    return client.post(url_for("Auth.login_view"), json=user_data)
-
-
-def get_access_token(client, user_data):
-    login_response = login_user(client, user_data)
-    assert login_response.status_code == 200, f"Login failed: {login_response.data}"
-    return login_response.get_json()["access_token"]
+@pytest.fixture(scope="function")
+def db(app):
+    yield _db
+    _db.session.rollback()
+    _db.session.remove()
